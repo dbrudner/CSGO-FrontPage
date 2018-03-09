07919f9fe4f07dc28ba51340b2d87406cdaa4144
dbrudner <dbrudne1@gmail.com>
2018-03-09 00:56:25 -0500

    commit

--- react/src/App.js
+++ react/src/App.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
-import TableList from './tableList'
-import SelectTable from './select-table'
 import axios from 'axios'
-import MatchesTable from './matches-table'
-import Select from './select'
 import _ from 'lodash'
+
+import Header from './header/header'
+import Button from './button/button'
 
 class App extends Component {
 
@@ -14,44 +13,8 @@ class App extends Component {
 		this.state = {
 			matches: [],
 			selectedMatches: [],
-			team: null,
-			teamMode: false,
-			eventMode: false
+			team: null
 		}
-	}
-
-	getValue = (key, value) => {
-
-		this.setState({[key]: value}, () => {
-			let allMatches = this.state.matches.filter(matchObj => {return matchObj.name === 'All Matches'})
-			console.log('value')
-			allMatches = allMatches[0].matches
-			if (key === 'team') {
-				console.log('teee')
-				const selectedMatches = allMatches.filter(match => {
-					if (match.team1) {
-						return match.team1.name === this.state.team || match.team2.name === this.state.team					
-					}
-				})
-
-				this.setState({selectedMatches})
-			}
-
-
-			if (key ==='event') {
-				console.log('he')
-				const selectedMatches = allMatches.filter(match => {
-					if (match.event) {
-						return match.event.name === this.state.event
-					}
-				})
-				console.log(selectedMatches)
-				this.setState({selectedMatches})				
-			}
-
-		})
-		
-		
 	}
 
 	getMatches = (route, name) => {
@@ -60,19 +23,6 @@ class App extends Component {
 			const matchesObject = {name, matches: matches.data.matches}
 			this.setState({matches: [...this.state.matches, matchesObject]})
 		})
-	}
-
-	getSelectTable = value => {
-		if (value === 'team') {
-			this.setState({teamMode: true})
-		} else if (value === 'event') {
-			this.setState({eventMode: true})
-		} else {
-			this.setState({teamMode: false, eventMode: false})
-		}
-
-		const selectedMatches = this.state.matches.filter(matchObj => matchObj.name === value)
-		this.setState({selectedMatches})
 	}
 
 	componentDidMount() {
@@ -104,32 +54,16 @@ class App extends Component {
 
 
 	render() {
-
-		if (this.state.matches.length > 2) {
-
-			if (this.state.teamMode || this.state.eventMode) {
-				const matches = {name: this.state.team, matches: this.state.selectedMatches}
-				console.log(matches)
-				return (
-					<div className="App">
-						<SelectTable value='Specific Team' getSelectTable={this.getSelectTable} matches={this.state.matches}/>
-						<Select valueName={this.state.teamMode ? 'team' : 'event'} getValue={this.getValue} options={this.state.teamMode ? this.state.teams : this.state.events} eventMode={this.state.eventMode} />
-						{this.state.team || this.state.event && this.state.selectedMatches.length ? <MatchesTable matches={[matches]} /> : null}
-					</div>
-				)
-			}
-
-			return (
-				<div className="App">
-					<SelectTable getSelectTable={this.getSelectTable} matches={this.state.matches}/>
-					{this.state.selectedMatches.length ? <MatchesTable matches={this.state.selectedMatches} /> : null}
-				</div>
-			);
-		}
-
-		else {
-			return <div>Loading</div>
-		}
+		return (
+			<div>
+				<Header/>
+				<div className='buttons'>
+					<Button btnClass='live' btnText='LIVE'/>
+					<Button btnClass='schedule' btnText='Schedule'/>
+					<Button btnClass='results' btnText='Results'/>
+				</div>				
+			</div>
+		)	
 	}
 }
 
---
+++ react/src/header/header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Header() {
+
+    return (
+        <div className='header text-center'>
+            <h1 className='header-title'>
+                CSGO
+            </h1>
+            <h3 className='header-description'>
+                Times and Results
+            </h3>
+        </div>
+    )
+}
