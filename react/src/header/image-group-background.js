b443af76982793e982208f3dcb9c4ced2eac6580
dbrudner <dbrudne1@gmail.com>
2018-03-09 02:18:45 -0500

    commit

--- react/src/header/image-group-background.js
+++ react/src/header/image-group-background.js
@@ -3,14 +3,13 @@ import React from 'react'
 export default function(props) {
     
     const imgCntr = {padding: '2rem'}
-    const img = {width: '100px', opacity: '.1'}
     const imageArray = {display: 'grid', gridTemplateColumns: 'auto auto auto auto', position: 'absolute'}
 
     const renderImages = () => {
         const images = props.srcArray.map(src => {
             return (
                 <div style={imgCntr}>
-                    <img style={img} src={src} />
+                    <img className='teamArrayImg' src={src} />
                 </div>
             )
         })
